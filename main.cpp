b7591e74ed79b7c84b3de4f6e0680c9b45a5a5bd
jyli <lijinyang161@mails.ucas.ac.cn>
2022-09-22 10:12:42 +0800

    raw encoder and decoder for fasta

--- main.cpp
+++ main.cpp
@@ -1,5 +1,8 @@
 #include <seqan/sequence.h>
 #include <seqan/index.h>
+#include <seqan/seq_io.h>
+#include <stdlib.h>
+#include <cstdlib>
 
 using namespace seqan;
 
@@ -52,17 +55,121 @@ void load2(const char* save_file_name) {
 }
 
 
+bool fmindex_encode(std::string fasta_path, std::string fmindex_path) {
+	SeqFileIn seqFileIn;
+
+	// Load fasta file
+	char abs_fasta_path[1024] = "";
+	if (_fullpath(abs_fasta_path, fasta_path.data(), 1024) == NULL || !open(seqFileIn, toCString(abs_fasta_path)))
+	{
+		std::cerr << "ERROR: Could not open the file.\n";
+		return false;
+	}
+
+	StringSet<CharString> ids;
+	StringSet<String<AminoAcid>> seqs;
+	try
+	{
+		readRecords(ids, seqs, seqFileIn);
+	}
+	catch (Exception const& e)
+	{
+		std::cout << "ERROR: " << e.what() << std::endl;
+		return false;
+	}
+
+	// Encode fasta file
+	for (unsigned i = 0; i < length(ids); ++i)
+		std::cout << ids[i] << '\t' << seqs[i] << '\n';
+
+	Index<StringSet<String<AminoAcid> >, FMIndex<> > fmIndex(seqs);
+	save(fmIndex, fmindex_path.data());
+
+	return true;
+}
+
+class FMIndexDecoder {
+private:
+	Index<StringSet<String<AminoAcid> >, FMIndex<> > fmIndex;
+
+public:
+	FMIndexDecoder(std::string fmindex_path) {
+		open(fmIndex, fmindex_path.data());
+	}
+
+	void decode(std::string& pattern) {
+		static Finder<Index<StringSet<String<AminoAcid>>, FMIndex<> > > fmFinder(fmIndex);
+
+		while (find(fmFinder, pattern))
+		{
+			std::cout << position(fmFinder).i1 << ", " << position(fmFinder).i2 << std::endl;
+		}
+		clear(fmFinder);
+	}
+
+	void decode(std::string pattern) {
+		static Finder<Index<StringSet<String<AminoAcid>>, FMIndex<> > > fmFinder(fmIndex);
+
+		while (find(fmFinder, pattern))
+		{
+			std::cout << position(fmFinder).i1 << ", " << position(fmFinder).i2 << std::endl;
+		}
+		clear(fmFinder);
+	}
+};
+
+
 int main()
 {
+	std::string fasta_str;
+	fasta_str = "demo_data/std8.fasta";
+	/*
+	CharString seqFileName;
+	SeqFileIn seqFileIn;
+	open(seqFileIn, "D:/demo_data/std8.fasta"); // input MUST be absolute path
+	close(seqFileIn);
+	open(seqFileIn, "D:/SeqAn_demo/demo_data/std8.fasta"); // input MUST be absolute path
+	close(seqFileIn);
+	open(seqFileIn, "D:/SeqAn_demo/demo_data/std8.FASTA"); // input MUST be absolute path
+	close(seqFileIn);
+
+	char abs_path[1024] = "";
+	_fullpath(abs_path, "demo_data/std8.fasta", 1024);
+	open(seqFileIn, toCString(abs_path));
+	close(seqFileIn);
+
+	seqFileName = getAbsolutePath("/demo_data/std8.fasta");
+	open(seqFileIn, toCString(seqFileName));
+	close(seqFileIn);
+
+	seqFileName = getAbsolutePath("./demo_data/std8.fasta");
+	open(seqFileIn, toCString(seqFileName));
+	close(seqFileIn);
+
+	char* fasta_path2 = "/demo_data/std8.fasta";
+	CharString seqFileName2 = getAbsolutePath(fasta_path2);
+	SeqFileIn seqFileIn2(toCString(fasta_path2));
+	char* fasta_path3 = "./demo_data/std8.fasta";
+	CharString seqFileName3 = getAbsolutePath(fasta_path3);
+	SeqFileIn seqFileIn3(toCString(fasta_path3));
+	*/
+
 	std::vector<std::string> proteins({ "VXLAGZ", "GKTVXL", "XLZ" });
 
 	std::string save_file_name_str;
-	save_file_name_str = "./tmp/fasta.esa";
+	save_file_name_str = "tmp/fasta.esa";
 	build1(proteins, save_file_name_str.data());
 	load1(save_file_name_str.data());
-	save_file_name_str = "./tmp/fasta.fm";
+	save_file_name_str = "tmp/fasta.fm";
 	build2(proteins, save_file_name_str.data());
 	load2(save_file_name_str.data());
+	save_file_name_str = "demo_data/std8.fm";
+
+	fmindex_encode(fasta_str, save_file_name_str);
+	FMIndexDecoder FMIndexDecoder(save_file_name_str);
+	FMIndexDecoder.decode("MRSLL");
+	FMIndexDecoder.decode("MGSIG");
+	FMIndexDecoder.decode("MKSTI");
 
 	return 0;
 }
