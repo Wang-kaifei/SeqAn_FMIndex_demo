e929e80e0a51f1910fd43fa80daa0e494f3bb432
jyli <lijinyang161@mails.ucas.ac.cn>
2022-09-22 11:06:50 +0800

    functions for decoder

--- main.cpp
+++ main.cpp
@@ -3,60 +3,15 @@
 #include <seqan/seq_io.h>
 #include <stdlib.h>
 #include <cstdlib>
+#include<iostream>
+#include<fstream>
 
 using namespace seqan;
 
 
-void build1(const std::vector<std::string>& proteins_list, const char* save_file_name) {
-	StringSet<String<AminoAcid> > protein_string_set;
-	for (auto& str : proteins_list) {
-		appendValue(protein_string_set, str);
-	}
-
-	Index<StringSet<String<AminoAcid> >, IndexEsa<> > fmIndex(protein_string_set);
-	save(fmIndex, save_file_name);
-}
-
-
-void load1(const char* save_file_name) {
-	Index<StringSet<String<AminoAcid> >, IndexEsa<> > fmIndex;
-	open(fmIndex, save_file_name);
-
-	Finder<Index<StringSet<String<AminoAcid>>, IndexEsa<> > > fmFinder(fmIndex);
-
-	while (find(fmFinder, "XL"))
-	{
-		std::cout << position(fmFinder).i1 << ", " << position(fmFinder).i2 << std::endl;
-	}
-}
-
-
-void build2(const std::vector<std::string>& proteins_list, const char* save_file_name) {
-	StringSet<String<AminoAcid> > protein_string_set;
-	for (auto& str : proteins_list) {
-		appendValue(protein_string_set, str);
-	}
-
-	Index<StringSet<String<AminoAcid> >, FMIndex<> > fmIndex(protein_string_set);
-	save(fmIndex, save_file_name);
-}
-
-
-void load2(const char* save_file_name) {
-	Index<StringSet<String<AminoAcid> >, FMIndex<> > fmIndex;
-	open(fmIndex, save_file_name);
-
-	Finder<Index<StringSet<String<AminoAcid>>, FMIndex<> > > fmFinder(fmIndex);
-
-	while (find(fmFinder, "XL"))
-	{
-		std::cout << position(fmFinder).i1 << ", " << position(fmFinder).i2 << std::endl;
-	}
-}
-
-
 bool fmindex_encode(std::string fasta_path, std::string fmindex_path) {
 	SeqFileIn seqFileIn;
+	std::cout << "Encoding..." << std::endl;
 
 	// Load fasta file
 	char abs_fasta_path[1024] = "";
@@ -78,12 +33,25 @@ bool fmindex_encode(std::string fasta_path, std::string fmindex_path) {
 		return false;
 	}
 
-	// Encode fasta file
-	for (unsigned i = 0; i < length(ids); ++i)
-		std::cout << ids[i] << '\t' << seqs[i] << '\n';
+	// Write IDs
+	std::ofstream  fmID;
+	try {
+		fmID.open((fmindex_path + ".id").data(), std::ios::out);
+	}
+	catch (Exception const& e)
+	{
+		std::cout << "ERROR: " << e.what() << std::endl;
+		return false;
+	}
 
+	for (unsigned i = 0; i < length(ids); ++i) {
+		fmID << ids[i] << std::endl;
+	}
+	fmID.close();
+
+	// Write indexed sequences
 	Index<StringSet<String<AminoAcid> >, FMIndex<> > fmIndex(seqs);
-	save(fmIndex, fmindex_path.data());
+	save(fmIndex, (fmindex_path + ".fm").data());
 
 	return true;
 }
@@ -91,85 +59,134 @@ bool fmindex_encode(std::string fasta_path, std::string fmindex_path) {
 class FMIndexDecoder {
 private:
 	Index<StringSet<String<AminoAcid> >, FMIndex<> > fmIndex;
+	std::vector<std::string> fmID;
+	std::vector<std::string> fmID_short;
 
 public:
 	FMIndexDecoder(std::string fmindex_path) {
-		open(fmIndex, fmindex_path.data());
+		std::cout << "Decoding..." << std::endl;
+		open(fmIndex, (fmindex_path + ".fm").data());
+
+		std::ifstream  f_fmID;
+		try {
+			f_fmID.open((fmindex_path + ".id").data(), std::ios::in);
+		}
+		catch (Exception const& e)
+		{
+			std::cout << "ERROR: " << e.what() << std::endl;
+			return;
+		}
+
+		std::string description;
+		while (getline(f_fmID, description)) {
+			fmID.emplace_back(description);
+
+			std::string::size_type pos;
+			pos = std::min(description.find('\t', 0), description.find(' ', 0));
+			if (pos < description.size())
+			{
+				description = description.substr(0, pos);
+			}
+			fmID_short.emplace_back(description);
+		}
+		f_fmID.close();
 	}
 
-	void decode(std::string& pattern) {
+	bool has_pattern(std::string pattern) {
 		static Finder<Index<StringSet<String<AminoAcid>>, FMIndex<> > > fmFinder(fmIndex);
+
+		if (find(fmFinder, pattern))
+		{
+			clear(fmFinder);
+			return true;
+		}
+		clear(fmFinder);
+		return false;
+	}
+
+	bool has_pattern(std::string& pattern) {
+		static Finder<Index<StringSet<String<AminoAcid>>, FMIndex<> > > fmFinder(fmIndex);
+
+		if (find(fmFinder, pattern))
+		{
+			clear(fmFinder);
+			return true;
+		}
+		clear(fmFinder);
+		return false;
+	}
+
+	int get_pattern_size(std::string pattern) {
+		static Finder<Index<StringSet<String<AminoAcid>>, FMIndex<> > > fmFinder(fmIndex);
+
+		int count = 0;
+		while (find(fmFinder, pattern))
+		{
+			count++;
+		}
+		clear(fmFinder);
+		return count;
+	}
+
+	int get_pattern_size(std::string& pattern) {
+		static Finder<Index<StringSet<String<AminoAcid>>, FMIndex<> > > fmFinder(fmIndex);
+
+		int count = 0;
+		while (find(fmFinder, pattern))
+		{
+			count++;
+		}
+		clear(fmFinder);
+		return count;
+	}
+
+	std::vector<std::pair<std::string, int>> get_all_pattern(std::string pattern) {
+		static Finder<Index<StringSet<String<AminoAcid>>, FMIndex<> > > fmFinder(fmIndex);
+		std::vector<std::pair<std::string, int>> res;
 
 		while (find(fmFinder, pattern))
 		{
-			std::cout << position(fmFinder).i1 << ", " << position(fmFinder).i2 << std::endl;
+			res.emplace_back(std::pair<std::string, int>(fmID_short[position(fmFinder).i1], position(fmFinder).i2));
 		}
 		clear(fmFinder);
+		return res;
 	}
 
-	void decode(std::string pattern) {
+	std::vector<std::pair<std::string, int>> get_all_pattern(std::string& pattern) {
 		static Finder<Index<StringSet<String<AminoAcid>>, FMIndex<> > > fmFinder(fmIndex);
+		std::vector<std::pair<std::string, int>> res;
 
 		while (find(fmFinder, pattern))
 		{
-			std::cout << position(fmFinder).i1 << ", " << position(fmFinder).i2 << std::endl;
+			res.emplace_back(std::pair<std::string, int>(fmID_short[position(fmFinder).i1], position(fmFinder).i2));
 		}
 		clear(fmFinder);
+		return res;
 	}
 };
 
 
 int main()
 {
-	std::string fasta_str;
-	fasta_str = "demo_data/std8.fasta";
-	/*
-	CharString seqFileName;
-	SeqFileIn seqFileIn;
-	open(seqFileIn, "D:/demo_data/std8.fasta"); // input MUST be absolute path
-	close(seqFileIn);
-	open(seqFileIn, "D:/SeqAn_demo/demo_data/std8.fasta"); // input MUST be absolute path
-	close(seqFileIn);
-	open(seqFileIn, "D:/SeqAn_demo/demo_data/std8.FASTA"); // input MUST be absolute path
-	close(seqFileIn);
-
-	char abs_path[1024] = "";
-	_fullpath(abs_path, "demo_data/std8.fasta", 1024);
-	open(seqFileIn, toCString(abs_path));
-	close(seqFileIn);
-
-	seqFileName = getAbsolutePath("/demo_data/std8.fasta");
-	open(seqFileIn, toCString(seqFileName));
-	close(seqFileIn);
-
-	seqFileName = getAbsolutePath("./demo_data/std8.fasta");
-	open(seqFileIn, toCString(seqFileName));
-	close(seqFileIn);
-
-	char* fasta_path2 = "/demo_data/std8.fasta";
-	CharString seqFileName2 = getAbsolutePath(fasta_path2);
-	SeqFileIn seqFileIn2(toCString(fasta_path2));
-	char* fasta_path3 = "./demo_data/std8.fasta";
-	CharString seqFileName3 = getAbsolutePath(fasta_path3);
-	SeqFileIn seqFileIn3(toCString(fasta_path3));
-	*/
-
-	std::vector<std::string> proteins({ "VXLAGZ", "GKTVXL", "XLZ" });
-
-	std::string save_file_name_str;
-	save_file_name_str = "tmp/fasta.esa";
-	build1(proteins, save_file_name_str.data());
-	load1(save_file_name_str.data());
-	save_file_name_str = "tmp/fasta.fm";
-	build2(proteins, save_file_name_str.data());
-	load2(save_file_name_str.data());
-	save_file_name_str = "demo_data/std8.fm";
+	std::string fasta_str = "demo_data/std8.fasta";
+	std::string save_file_name_str = "tmp/std8";
 
 	fmindex_encode(fasta_str, save_file_name_str);
 	FMIndexDecoder FMIndexDecoder(save_file_name_str);
-	FMIndexDecoder.decode("MRSLL");
-	FMIndexDecoder.decode("MGSIG");
-	FMIndexDecoder.decode("MKSTI");
+
+	std::cout << "Checking MRSLL..." << std::endl;
+	std::cout << FMIndexDecoder.has_pattern("MRSLL") << std::endl;
+	std::cout << FMIndexDecoder.get_pattern_size("MRSLL") << std::endl;
+	for (auto& res : FMIndexDecoder.get_all_pattern("MRSLL")) {
+		std::cout << res.first << ", " << res.second << std::endl;
+	}
+
+	std::cout << "Checking K..." << std::endl;
+	std::cout << FMIndexDecoder.has_pattern("K") << std::endl;
+	std::cout << FMIndexDecoder.get_pattern_size("K") << std::endl;
+	for (auto& res : FMIndexDecoder.get_all_pattern("K")) {
+		std::cout << res.first << ", " << res.second << std::endl;
+	}
 
 	return 0;
 }
